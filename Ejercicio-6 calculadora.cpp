490a13e933e32e2c1e2db62b4c05500bac617338
SofiaNavarrete20 <98790035+SofiaNavarrete20@users.noreply.github.com>
2022-02-01 17:54:59 -0600

    Add files via upload
    Agregar ejercicio 6

---
+++ Ejercicio-6 calculadora.cpp
@@ -0,0 +1,38 @@
+/*
+
+ 
+  
+
+
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+  
+int main()
+{
+	 int a,b,   suma=0, resta=0, multiplicacion=0, division=0;
+   
+   cout <<" Inserte un digito: "; 
+   cin>>a;
+   cout <<" Inserte otro digito: "; 
+   cin>>b;
+   
+   suma= a+b;
+   resta=a-b;
+   multiplicacion=a*b;
+   division= a/b;
+   
+   cout << "La suma es:  "<<suma<<endl;
+   cout << "La resta es: "<<resta<<endl;
+   cout << "La multiplicacion es:  "<<multiplicacion<<endl;
+   cout << "La division es:  "<<division<<endl;
+   
+   
+   		
+  return 0;	
+}
+
+
+  
