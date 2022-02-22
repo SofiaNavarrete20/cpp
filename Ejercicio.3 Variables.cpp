92fe277b32c460e1f427a500d4cb97ea4f1c1f65
SofiaNavarrete20 <98790035+SofiaNavarrete20@users.noreply.github.com>
2022-02-22 15:29:25 -0600

    Agregando ejercicio 3

---
+++ Ejercicio.3 Variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	 int numeroEntero;
+	numeroEntero= 15;
+	
+	cout << "Entero:  " << numeroEntero <<endl;
+	
+	numeroEntero= 18;
+	
+	cout << "Entero modificado: " << numeroEntero << endl;
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
