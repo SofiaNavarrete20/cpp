21f472b2ff5f9030fe7fcbfb75adb123049c1382
SofiaNavarrete20 <98790035+SofiaNavarrete20@users.noreply.github.com>
2022-03-16 17:31:59 -0600

    Add files via upload
    Agregando tarea 6

--- Tarea # 6/Productos.cpp
+++ Tarea # 6/Productos.cpp
@@ -1,74 +1,107 @@
 #include <iostream>
 
-
-
 using namespace std;
 
-extern void agregarProducto (string descripcion, int cantidad, double precio)
+extern void agregarProducto(string descripcion, int cantidad, double precio);
 
-void productos (int opcion)
+void productos(int opcion)
 {
-	system ("cls");
-	int opcionProducto = 0;
-	
-      switch (opcion)
-	  {
-	  	     case 1:
-	  	     cout <<endl;
-		     cout << "BEBIDAS CALIENTES" <<endl;
-	         cout << "*****************" <<endl;
-	         cout << "1 Chocolate caliente" <<endl;
-	         cout << "2 Te chai" << endl;
-	         cout << "Ingrese la opcion de su preferencia: ";
-	         cin  >>opcionProducto;
-	         
-	         switch
-			 {
-			 	case 1
-			 	agregarProducto ("1 Chocolate caliente - L.38.00 , 1,38");
-			 	break;
-			 	
-			 	case 2
-			 	agregarProducto ("2 Te de chai- L.45.00, 2, 45");
-			 	break;
-			 	
-			 	
-			 	default:
-				 {
-				 cout << endl;
-				 cout << "OPCION NO VALIDA"	<< endl;
-				 
-				 system ("pause");
-				 return ;
-				 break;
-				 
-			 	
-			 }
-			  
-			  cout << endl;
-			  cout << "Producto agregado con exito " << endl;
-			  system ("pause");
-			  
-		  	 
-			default:
-				cout << "OPCION NO VALIDA"	<< endl;
-				system ("pause");
-				 return ;
-				 break;
-				
-		  break;
-			
-			    
-			 	
-		  
-	  }
-}
-	
-	
-	
-	
-}
+    system("cls");
 
+    int opcionProducto = 0;
+
+    switch (opcion)
+    {
+    case 1:
+    {
+        cout << "BEBIDAS CALIENTES" << endl;
+        cout << "*****************" << endl;
+        cout << "1 - Te de chai" << endl;
+        cout << "2 - chocolate caliente" << endl;
+        cout << endl;
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto("1 Te de chai - L 40.00", 1, 40);
+            break;
+        case 2:
+            agregarProducto("1 chocolate caliente - L 30.00", 1, 30);
+            break;        
+        default:
+            {
+                cout << "OPCION NO VALIDA";
+                return;
+                break;
+            }
+        }
+
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
+        system("pause");
+        
+        break;
+    }
+    case 2:
+    {
+        cout << "BEBIDAS FRIAS" << endl;
+        cout << "*************" << endl;
+        cout << "1 - Mochaccino" << endl;
+        cout << "2 - granita de mora" << endl;
+        cout << "3 - Frappe caramelo" << endl;
+        cout << endl;
+        
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+        
+        if(opcionProducto == 1){
+        	agregarProducto("1 Mochaccino - L 63.00", 1, 63);
+		} else if(opcionProducto == 2){
+			agregarProducto("1 granita de mora - L 52", 1, 52);
+		} else if(opcionProducto == 3){
+			agregarProducto("1 Frappe caramelo - L 80.00", 1, 80);
+		} else {
+			cout << "OPCION NO VALIDA" << endl;
+			return;
+		}
+        
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
+        system("pause");
+        break;
+    }
+    case 3:
+    {
+        cout << "REPOSTERIA" << endl;
+        cout << "**********" << endl;
+        cout << "1 - Dona" << endl;
+        cout << "2 - rollos de canela" << endl;
+        cout << endl;
+        
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+        
+        if(opcionProducto == 1){
+        	agregarProducto("1 Dona - L 20.00", 1, 20);
+		} else if(opcionProducto == 2){
+			agregarProducto("1  rollos de canela - L 30.00", 1, 30);
+		} else {
+			cout << "OPCION NO VALIDA" << endl;
+			return;
+		}
+        
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
+        system("pause");
+        break;
+    }        
+    default:
+        break;
+    }
+}
 
 
 
