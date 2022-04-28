7161c41b6a2f7ff069e41defeffd8220c0d7c23f
SofiaNavarrete20 <98790035+SofiaNavarrete20@users.noreply.github.com>
2022-04-27 23:55:31 -0600

    Add files via upload
    proyecto final

---
+++ DepreciacionEdificio.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cmath>		// Libreria que permite el uso de funciones matematicas
+#include <stdlib.h> 	// Libreria que permite el uso de system("pause")
+
+using namespace std;
+int main()
+{
+	float subtotal=0, valor=0, anos=0;
+	float total=0, costo = 0;
+	
+        
+	
+	if( valor/anos)
+	{
+	    	costo=0.01;
+	        anos=25; 
+	    	total= valor-costo;
+        	subtotal=total/anos;
+		
+		cout<<" Ingrese el valor residual del costo del primer edificio: ";
+	    cin>>valor;
+	    cout<<" El total de depreciacion es:"<<subtotal<<endl <<endl;
+	}
+	 
+	 
+	 
+	 	if( valor/anos)
+	{
+		
+		    costo=0.02;
+	        anos=30; 
+	        total= valor-costo;
+        	subtotal=total/anos;
+        	
+		    cout<<" Ingrese el valor residual del costo del segundo edificio: ";
+	        cin>>valor;
+	        cout<<" El total de depreciacion es:"<<subtotal <<endl <<endl;
+	}
+	 
+	 
+	 	if( valor/anos)
+	{
+		    costo=0.03;
+	        anos=40; 
+	        total= valor-costo;
+        	subtotal=total/anos;
+		
+		    cout<<" Ingrese el valor residual del costo del tercer edificio: ";
+	        cin>>valor;
+	        cout<<" El total de depreciacion es:"<<subtotal <<endl <<endl;
+	}
+	 
+	 	if( valor/anos)
+	{
+		    costo=0.04;
+	        anos=26; 
+	        total= valor-costo;
+        	subtotal=total/anos;
+		
+		    cout<<" Ingrese el valor residual del costo del cuarto edificio: ";
+	        cin>>valor;
+	        cout<<" El total de depreciacion es:"<<subtotal <<endl <<endl;
+	}
+	 
+	 	if( valor/anos)
+	{
+		    costo=0.05;
+	        anos=15; 
+	        total= valor-costo;
+        	subtotal=total/anos;
+		
+		    cout<<" Ingrese el valor residual del costo del quinto edificio: ";
+	        cin>>valor;
+	        cout<<" El total de depreciacion es:"<<subtotal <<endl <<endl;
+	}
+	 
+
+	
+	system("pause");
+	
+}
